3dc3f9205c0e97ddffdcd7ac1dea27489c598934
Christian Santos Martorano <christiansmartorano@gmail.com>
2023-04-03 17:20:59 -0300

    typo: adjusted the name of the column

--- src/sql/create_addresses_table.sql
+++ src/sql/create_addresses_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE addresses (
     id SERIAL PRIMARY KEY,
     city VARCHAR(255) NOT NULL,
-    provincy VARCHAR(2) NOT NULL,
+    province VARCHAR(2) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW(),
 );
